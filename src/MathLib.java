e5c898fb870525e3dd28d34250e8d0d3d182d537
Vladimír Hruban <Hruban.Vladimir@gmail.com>
2016-04-06 19:11:52 +0200

    added MathLib.java

---
+++ src/MathLib.java
@@ -0,0 +1,29 @@
+public class MathLib {
+    double plus (double a, double b) {
+        return 0;
+    }
+
+    double minus (double a, double b) {
+        return 0;
+    }
+
+    double multiply (double a, double b) {
+        return 0;
+    }
+
+    double divide (double a, double b) {
+        return 0;
+    }
+
+    double exponent (double a, double b) {
+        return 0;
+    }
+
+    long factorial (int a) {
+        return 0;
+    }
+
+    double modulo (int a, int b) {
+        return 0;
+    }
+}
--- src/testMathLib.java
+++ src/testMathLib.java
@@ -1,6 +1,3 @@
-/**
- *
- */
 public class TestMathLib {
 
     private MathLib toTest;

ce329bc2fe4d8016c8e7bb50f9cb5a9f64e6b6a9
Vladimír Hruban <Hruban.Vladimir@gmail.com>
2016-04-06 18:58:51 +0200

    rerefactored test class

--- src/testMathLib.java
+++ src/testMathLib.java
@@ -1,5 +1,16 @@
 /**
  *
  */
-public class testMathLib {
+public class TestMathLib {
+
+    private MathLib toTest;
+
+    private void prepare() {
+        toTest = new MathLib();
+    }
+
+    // adds two positive numbers
+    public void testPlusPositive() {
+        prepare();
+    }
 }

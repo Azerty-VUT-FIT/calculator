c075cf1f3c8311ca489188be7061994cf4ea4a17
vladimirhruban <hruban.vladimir@gmail.com>
2016-04-24 18:35:54 +0200

    calc 0.9.2

---
+++ src/calculator/calculator-1.0/debian/calculator.default.ex
@@ -0,0 +1,10 @@
+# Defaults for calculator initscript
+# sourced by /etc/init.d/calculator
+# installed at /etc/default/calculator by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
